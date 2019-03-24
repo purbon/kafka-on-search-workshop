7c6f12e5ee7220e6bb7a1e589d421090989692d8
Pere Urbon <pere@confluent.io>
2019-03-24 16:06:45 +0100

    [Kafka Connect] add customer profiles to be indexed into elasticsearch

--- scripts/create-elasticsearch-sink.sh
+++ scripts/create-elasticsearch-sink.sh
@@ -18,3 +18,22 @@ curl -i -X POST -H "Accept:application/json" \
           "value.converter.schemas.enable": false
        }
     }'
+
+    curl -i -X POST -H "Accept:application/json" \
+        -H  "Content-Type:application/json" http://kafka-connect-cp:18083/connectors/ \
+        -d '{
+          "name": "elastic-sink-profiles",
+          "config": {
+              "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+              "tasks.max": "1",
+              "topics": "customers-profiles",
+              "key.ignore": "true",
+              "connection.url": "http://elasticsearch:9200",
+              "type.name": "customers",
+              "name": "elastic-sink-profiles",
+              "schema.ignore": "true",
+              "key.converter":"org.apache.kafka.connect.storage.StringConverter",
+              "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+              "value.converter.schemas.enable": false
+           }
+        }'
